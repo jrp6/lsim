7c004ed4a54ca8c718cc9ed23479047e5cac7ceb
Juho Rinta-Paavola <juho.s.rp@gmail.com>
2015-06-10 16:43:53 +0300

    Add deck dealing

--- main.hs
+++ main.hs
@@ -1,5 +1,10 @@
 -- lsim - läpsy simulator - Juho Rinta-Paavola 2015
 import System.Random
+import Data.List
+
+(??) :: [a] -> [Int] -> [a]
+xs ?? []     = []
+xs ?? (i:is) = (xs !! i):(xs ?? is)
 
 data Suit = Club | Diamond | Heart | Spade
           deriving (Read, Show, Enum, Eq, Ord)
@@ -26,3 +31,11 @@ mixedDeck :: Int -> Deck
 mixedDeck seed = map toEnum random52
   where random52 = take 52 $ randomRs (0,51) gen
         gen      = mkStdGen seed
+
+deal :: Deck -> Int -> [Deck]
+deal deck n
+  | n == 1    = [deck]
+  | n < 1     = []
+  | otherwise = thisHand:(deal (deck \\ thisHand) (n-1))
+  where thisHand = deck ?? [0,n .. (length deck)-1]
+
