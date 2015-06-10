e1fa99b3efe943f42b24627f7c78f6de63bf47d5
Juho Rinta-Paavola <juho.s.rp@gmail.com>
2015-06-10 18:30:37 +0300

    Implement the game except for face cards

--- main.hs
+++ main.hs
@@ -6,6 +6,10 @@ import Data.List
 xs ?? []     = []
 xs ?? (i:is) = (xs !! i):(xs ?? is)
 
+replaceIndex :: [a] -> Int -> a -> [a]
+replaceIndex (x:xs) 0 new = new:xs
+replaceIndex (x:xs) n new = x:(replaceIndex xs (n-1) new)
+
 data Suit = Club | Diamond | Heart | Spade
           deriving (Read, Show, Enum, Eq, Ord)
 data CardValue = Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten | Jack | Queen | King | Ace
@@ -19,7 +23,7 @@ instance Ord Card where
   compare c1 c2 = compare (value c1, suit c1) (value c2, suit c2)
 
 instance Enum Card where
-  toEnum n = let (v,s) = n `divMod` 4 in Card (toEnum v) (toEnum s)
+  toEnum n = let (v,s) = n `quotRem` 4 in Card (toEnum v) (toEnum s)
   fromEnum c = fromEnum (value c) * 4 + fromEnum (suit c)
 
 type Deck = [Card]
@@ -38,4 +42,18 @@ deal deck n
   | n < 1     = []
   | otherwise = thisHand:(deal (deck \\ thisHand) (n-1))
   where thisHand = deck ?? [0,n .. (length deck)-1]
+
+playRound :: [Deck] -> Deck -> StdGen -> Integer -> Int -> Integer
+playRound hands table g roundNo playerNo
+  | length (filter (not.null) hands) == 1 = roundNo
+  | (length table) >= 2 && value (last table) == value (last $ init table) =
+    playRound (appendTabletoPlayer rand) [] nextG nextRound rand
+  | otherwise =
+    playRound (removePlayerCard playerNo) (table ++ [head $ hands !! playerNo]) g nextRound nextPlayer
+  where n                      = length hands
+        (rand,nextG)           = randomR (0,n-1) g
+        appendTabletoPlayer pl = replaceIndex hands pl $ (hands !! pl) ++ table
+        removePlayerCard pl    = replaceIndex hands pl $ tail $ hands !! pl
+        nextRound              = roundNo + 1
+        nextPlayer             = (playerNo + 1) `rem` n
 
